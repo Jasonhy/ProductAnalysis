980e564f1423a5035a509ca41d3c13076e965cdc
jasonhy <840181989@qq.com>
2017-07-12 01:16:31 +0800

    数据分析

--- ProductAnalysis/urls.py
+++ ProductAnalysis/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/',views.index),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+import pandas as pd
+import json
+import codecs
+import re
+
 
 # Create your views here.
+
+def index(request):
+    with codecs.open('/static/file/product_info.json', 'r',encoding='utf-8') as file:
+        datas = json.load(file)
+
+    temps = datas[0]['p_c_all_nums']
+    values = []
+    keys = []
+    for i in range(len(temps)):
+        if i % 2 == 0:
+            keys.append(temps[i])
+        else:
+            values.append(int(re.search(r"(\d+)", temps[i]).group(0)))
+    df = pd.DataFrame(values, index=keys)
+
+
+    return render(request,'index.html')
