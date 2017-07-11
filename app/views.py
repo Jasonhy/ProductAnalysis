12d13938e6c6cb54ba5cb70dd6a24fe353589e3d
jasonhy <840181989@qq.com>
2017-07-12 01:43:30 +0800

    数据分析

--- app/views.py
+++ app/views.py
@@ -3,12 +3,16 @@ import pandas as pd
 import json
 import codecs
 import re
+import matplotlib.pyplot as plt
+from matplotlib.pylab import mpl
 
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+mpl.rcParams['font.serif'] = ['SimHei']
 
 # Create your views here.
 
 def index(request):
-    with codecs.open('/static/file/product_info.json', 'r',encoding='utf-8') as file:
+    with codecs.open('static/file/product_info.json', 'r',encoding='utf-8') as file:
         datas = json.load(file)
 
     temps = datas[0]['p_c_all_nums']
@@ -20,6 +24,6 @@ def index(request):
         else:
             values.append(int(re.search(r"(\d+)", temps[i]).group(0)))
     df = pd.DataFrame(values, index=keys)
-
-
+    df.plot()
+    plt.savefig("01.png")
     return render(request,'index.html')
