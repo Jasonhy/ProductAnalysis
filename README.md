190ec4968fb7b9f4a9e1732d5db29a8d4114588e
Jasonhy <840181989@qq.com>
2017-07-20 14:46:20 +0800

    分析视图改为bokeh

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # ProductAnalysis
+[python必备知识点](#./doc/python必备知识点.md)
 ##  首页
 ![](./screen/index.png)
 ##  分析结果页
--- app/tests.py
+++ app/tests.py
@@ -2,8 +2,6 @@ from django.test import TestCase
 
 # Create your tests here.
 
-from django.shortcuts import HttpResponse
+def test():
 
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return "Hello World"
+    pass
